7e2265ebd1b975e13d23a24c73323e4de01291f3
wbr055 <87501735+wbr055@users.noreply.github.com>
2021-07-15 14:33:49 -0700

    Add files via upload

---
+++ src/components.js
@@ -0,0 +1,39 @@
+// displays the AppObject
+export const AppCard = ({name, status}) => (
+    <div className={'p1 flex aic'}>
+        <div className={'statusCircle'} style={{background: status}}/>
+        <div className={'ml1'}>
+            {name}
+        </div>
+    </div>
+);
+
+// manually fetch the apps to refresh the data
+// indicates the refreshing state
+export const RefreshButton = ({onClick, refreshing}) => (
+    <div className={'pl1'}>
+        <button className={'btn'} onClick={onClick}>
+            Refresh
+        </button>
+        {refreshing && <span className={'pl1'}>Refreshing...</span>}
+    </div>
+);
+
+
+const CheckBox = ({label, checked, onClick}) => (
+    <label className={'p1 mr1 flex aic'}>
+        <div>{label}</div>
+        <input type={'checkbox'} onClick={onClick} defaultChecked={checked}/>
+    </label>
+);
+
+
+export const Filters = ({statusFilters = {}, toggleStatusFilter = () => ({})}) => (
+    <div className={'p1 flex aic'}>
+        {Object.keys(statusFilters).map(color => (
+            <CheckBox key={color} label={color} checked={statusFilters[color]} onClick={() => {
+                toggleStatusFilter(color)
+            }}/>
+        ))}
+    </div>
+);
---
+++ src/index.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {getApps} from "./api";
+import {AppCard, RefreshButton, Filters} from "./components";
+
+/**
+ * @typedef {{green: boolean, yellow:boolean, red:boolean}} statusFilters
+ */
+
+/**
+ *
+ * @param {AppObject[]} appList
+ * @param {statusFilters} statusFilters
+ * @returns {[AppObject]} the filtered appList
+ */
+const filterAppListByStatus = (appList = [], statusFilters = {}) => {
+    return appList
+}
+
+
+/**
+ * This custom hook controls the filter selection state and applies the filters to the appList
+ * @param {AppObject[]} appList - accepts the list returned from getApps;
+ * @returns {{toggleStatusFilter: {Function(color:string)}, statusFilters: {statusFilters}, filteredList: {AppObject[]} }}
+ */
+const useFilteredListState = (appList = []) => {
+
+    const [statusFilters, setStatusFilters] = useState({
+        green: true, yellow: true, red: true
+    });
+
+
+    const filteredList = filterAppListByStatus(appList, statusFilters);
+
+    /**
+     * toggle the boolean and set the status filter state
+     * @param {string} color - accepts the filter color based on the checkbox that was clicked
+     */
+    const toggleStatusFilter = color => {
+        //setStatusFilters
+    };
+
+    return {filteredList, toggleStatusFilter, statusFilters}
+
+}
+
+
+const AppComponent = () => {
+    const [{data, fetching, error}, seteState] = useState({
+        data: undefined, fetching: false, error: undefined
+    });
+
+    const {filteredList, toggleStatusFilter, statusFilters} = useFilteredListState(data);
+
+    const fetchApps = async () => {
+        // state should be updated to indicate the progress of the fetch cycle
+
+        // call getApps() and set the data to state
+
+        // make sure to catch any errors that occur
+    }
+
+    /*
+        use the proper react hook to call fetchApps() when the component mounts
+     */
+
+    /*
+        The user can initiate a refresh of data by clicking the refresh button.
+        Determine a condition to indicate to the user that data is refreshing.
+        it should only indicate refreshing for subsequent requests after the initial fetch cycle.
+    */
+    const isRefreshing = false;
+
+    return (
+        <div className={'p2'}>
+            <h1>App Statuses (React Interview)</h1>
+            <RefreshButton
+
+                refreshing={isRefreshing}
+                onClick={fetchApps}/>
+
+            <Filters
+                toggleStatusFilter={toggleStatusFilter}
+                statusFilters={statusFilters}/>
+
+            <div className={'flex flexWrap'}>
+                {/*
+
+                    render the filteredList here using the <AppCard/> component for each item in the list
+
+                    show "loading..." while data is being fetched
+
+                    and indicate when there is an error
+
+
+                */}
+            </div>
+
+        </div>
+    )
+
+}
+
+ReactDOM.render(<AppComponent/>, document.getElementById('root'));
+
